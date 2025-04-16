28f50a9b49ee0940913694ee0e482446b16e05ae
Jay <jaydwivedi.programmer@gmail.com>
2025-04-16 16:49:12 -0400

    Session 3

--- Auth_app/admin.py
+++ Auth_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import Contact
 
 # Register your models here.
+
+# admin.site.register(Contact)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display=['id','name','email','message']
+    search_fields=['email','name']
+    list_filter=['email','name']
--- Auth_app/models.py
+++ Auth_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Contact(models.Model):
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=40,unique=True)
+    message=models.TextField(max_length=200)
