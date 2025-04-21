e10333811b822d25d89f03d3c62cd77461ea14d0
Jay <jaydwivedi.programmer@gmail.com>
2025-04-20 23:49:57 -0400

    Session 5

--- Auth_app/urls.py
+++ Auth_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('p-h/',views.print_hello),
     path('home/',views.homepage),
     path('data/',views.all_data),
+    path('sud/<int:pk>/',views.single_user_data)
 ]
--- Auth_app/views.py
+++ Auth_app/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from . models import Contact
 from .serializers import ContactSerializer
+from django.views.decorators.csrf import csrf_exempt
+
+import json
 
 # Create your views here.
 
@@ -11,6 +14,7 @@ def print_hello(request):
 def homepage(request):
     return render(request,"Auth_app/index.html")
 
+@csrf_exempt
 def all_data(request):
     if request.method=="GET":
         all_user_data=Contact.objects.all() #queryset
@@ -19,3 +23,91 @@ def all_data(request):
             "success":True,
             "data":sd.data
         },status=200)
+    
+    if request.method=='POST':
+        input_data=json.loads(request.body) #json
+        # json -> qs
+        sd=ContactSerializer(data=input_data) 
+        if sd.is_valid():
+            sd.save()
+            return JsonResponse({
+                "success":True,
+                "message":"Data Saved successfully"
+            },status=201)
+
+@csrf_exempt
+def single_user_data(request,pk):
+
+    if request.method=="GET":
+        try:
+            user=Contact.objects.get(pk=pk) #single object in a QS
+            sd=ContactSerializer(user) #serilaization
+            return JsonResponse({
+               " success":True,
+               "data":sd.data
+            },status=200)
+        
+        except Exception as e:
+            return JsonResponse({
+                "Success":False,
+                "Error":str(e)
+            },status=500)
+        
+    if request.method=="PUT":
+        try:
+            input_data=json.loads(request.body)
+            user=Contact.objects.get(pk=pk)
+
+            sd=ContactSerializer(user,data=input_data)
+
+            if sd.is_valid():
+                sd.save()
+                return JsonResponse({
+                    "Success":True,
+                    "Message":"user updated successfully"
+                },status=200)
+            
+            return JsonResponse(sd.errors, status=400)
+            
+        except Exception as e:
+            return JsonResponse({
+                "Success":False,
+                "Error": str(e.as_json())
+            },status=500)
+        
+    if request.method=="PATCH":
+        try:
+            input_data=json.loads(request.body)
+            user=Contact.objects.get(pk=pk)
+
+            sd=ContactSerializer(user,data=input_data,partial=True)
+
+            if sd.is_valid():
+                sd.save()
+                return JsonResponse({
+                   "success":True,
+                   "Message":"Partial update is Done"
+                },status=200)
+            return JsonResponse(sd.errors, status=400)
+        except user.DoesNotExist:
+            return JsonResponse({
+                "Success":False,
+                "Message":"User Doesn't exist",
+                "PK":pk
+            }, status=404)
+        except Exception as e:
+            return JsonResponse({
+                "Success":False,
+                "Error": str(e.as_json())
+            },status=500)
+        finally:
+            return JsonResponse({
+               "Message":"Hii You have reached here"
+            })
+        
+    if request.method=="DELETE":
+        user=Contact.objects.get(pk=pk)
+        user.delete()
+        return JsonResponse({
+           "success":True
+        })
